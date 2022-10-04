99f3e69f46245066ce0d0e6ce25c16c53ca678cc
Rinon <rinon.ten@filament.uk.com>
2022-10-04 17:03:55 +0300

    Add signup and login forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
-import { Switch, Route} from 'react-router-dom';
+import { Box } from '@mui/material';
 import Header from './components/Header';
 
 const App = () => {
   return (
-    <div>
-       <Header />
-    </div>
+    <Box>
+      <Header/>
+      <Box sx={{background: '#f2f3f8'}}>
+
+      </Box>
+    </Box>
   );
 }
 
